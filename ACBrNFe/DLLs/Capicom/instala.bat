7181f569a825fd9153ff9c6f9bcf4baf842afd4d
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2009-07-28 18:47:13 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@231 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- ACBrNFe/DLLs/Capicom/instala.bat
+++ ACBrNFe/DLLs/Capicom/instala.bat
@@ -2,10 +2,25 @@
 
 ECHO *** Copiando as DLLs ***
 if NOT EXIST %windir%\System32\capicom.dll copy capicom.dll %windir%\System32
+if EXIST %windir%\System32\capicom.dll echo CAPICOM.DLL copiada com Sucesso!
+if NOT EXIST %windir%\System32\capicom.dll echo Nao foi possivel copiar CAPICOM.DLL!
+
 if NOT EXIST %windir%\System32\msxml5.dll  copy msxml5.dll  %windir%\System32
+if EXIST %windir%\System32\msxml5.dll echo MSXML5.DLL copiada com Sucesso!
+if NOT EXIST %windir%\System32\msxml5.dll echo Nao foi possivel copiar MSXML5.DLL!
+
 if NOT EXIST %windir%\System32\msxml5r.dll copy msxml5r.dll %windir%\System32
+if EXIST %windir%\System32\msxml5r.dll echo MSXML5R.DLL copiada com Sucesso!
+if NOT EXIST %windir%\System32\msxml5r.dll echo Nao foi possivel copiar MSXML5R.DLL
+
+ECHO .
 
 ECHO *** Registrando as DLLs ***
-regsvr32 %windir%\System32\capicom.dll /s
-regsvr32 %windir%\System32\msxml5.dll /s
-regsvr32 %windir%\System32\msxml5r.dll /s
+ECHO Em caso de problemas no registro das DLL's, verifique se possui 
+ECHO direitos de Administrador sobre o prompt do MS-DOS.
+regsvr32 %windir%\System32\capicom.dll 
+regsvr32 %windir%\System32\msxml5.dll 
+
+ECHO .
+
+pause

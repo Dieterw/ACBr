be7c9b71b28d69294499010859bbf6bcc7524ff3
anfm <anfm@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2009-08-21 18:20:13 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@302 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- ACBrNFePCN/pcnRetConsCad.pas
+++ ACBrNFePCN/pcnRetConsCad.pas
@@ -111,6 +111,19 @@ type
     FUF: string;
     FcSit: integer;
     FxNome: string;
+    FxFant: String;
+    FxRegApur:String;
+    FCNAE:Integer;
+    FdIniAtiv:TDateTime;
+    FdUltSit:TDateTime;
+    FdBaixa:TDateTime;
+    FxLgr:String;
+    Fnro:String;
+    FxCpl:String;
+    FxBairro:String;
+    FcMun:Integer;
+    FxMun:String;
+    FCep:Integer;
   published
     property IE: string read FIE write FIE;
     property CNPJ: string read FCNPJ write FCNPJ;
@@ -118,6 +131,19 @@ type
     property UF: string read FUF write FUF;
     property cSit: integer read FcSit write FcSit;
     property xNome: string read FxNome write FxNome;
+    property xFant: string read FxFant write FxFant;
+    property xRegApur: string read FxRegApur write FxRegApur;
+    property CNAE: Integer read FCNAE write FCNAE;
+    property dIniAtiv: TDateTime read FdIniAtiv write FdIniAtiv;
+    property dUltSit: TDateTime read FdUltSit write FdUltSit;
+    property dBaixa: TDateTime read FdBaixa write FdBaixa;
+    property xLgr: string read FxLgr write FxLgr;
+    property nro: string read Fnro write Fnro;
+    property xCpl: string read FxCpl write FxCpl;
+    property xBairro: string read FxBairro write FxBairro;
+    property cMun: Integer read FcMun write FcMun;
+    property xMun: string read FxMun write FxMun;
+    property CEP: Integer read FCep write FCep;
   end;
 
 implementation
@@ -196,6 +222,22 @@ begin
         (*GR11*)InfCad[i].FUF := Leitor.rCampo(tcStr, 'UF');
         (*GR12*)InfCad[i].FcSit := Leitor.rCampo(tcInt, 'cSit');
         (*GR13*)InfCad[i].FxNome := Leitor.rCampo(tcStr, 'xNome');
+
+        {Acrescentado por João Paulo}
+        (*GR13a*)InfCad[i].FxFant := Leitor.rCampo(tcStr, 'xFant');
+        (*GR14*)InfCad[i].FxRegApur := Leitor.rCampo(tcStr, 'xRegApur');
+        (*GR15*)InfCad[i].FCNAE := Leitor.rCampo(tcInt, 'CNAE');
+        (*GR16*)InfCad[i].FdIniAtiv := Leitor.rCampo(tcDat, 'dIniAtiv');
+        (*GR17*)InfCad[i].FdUltSit := Leitor.rCampo(tcDat, 'dUltSit');
+        (*GR18*)InfCad[i].FdBaixa := Leitor.rCampo(tcDat, 'dBaixa');
+        (*GR23*)InfCad[i].FxLgr := Leitor.rCampo(tcStr, 'xLgr');
+        (*GR24*)InfCad[i].Fnro := Leitor.rCampo(tcStr, 'nro');
+        (*GR25*)InfCad[i].FxCpl := Leitor.rCampo(tcStr, 'xCpl');
+        (*GR26*)InfCad[i].FxBairro := Leitor.rCampo(tcStr, 'xBairro');
+        (*GR27*)InfCad[i].FcMun := Leitor.rCampo(tcInt, 'cMun');
+        (*GR28*)InfCad[i].FxMun := Leitor.rCampo(tcStr, 'xMun');
+        (*GR29*)InfCad[i].FCep := Leitor.rCampo(tcInt, 'CEP');
+
         inc(i);
       end;
       Result := True;

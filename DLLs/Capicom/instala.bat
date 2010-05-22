6cd6f0b480eb033fe6895b2e8e8aea2ee8ef7510
anfm <anfm@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2010-05-22 21:38:07 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@1242 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- DLLs/Capicom/instala.bat
+++ DLLs/Capicom/instala.bat
@@ -10,7 +10,6 @@ if NOT EXIST %windir%\System32\msxml5r.dll copy msxml5r.dll %windir%\System32
 ECHO *** Registrando as DLLs ***
 regsvr32 %windir%\System32\capicom.dll /s
 regsvr32 %windir%\System32\msxml5.dll /s
-regsvr32 %windir%\System32\msxml5r.dll /s
 goto end
 
 :Win64
@@ -21,7 +20,6 @@ if NOT EXIST %windir%\SysWOW64\msxml5r.dll copy msxml5r.dll %windir%\SysWOW64
 ECHO *** Registrando as DLLs x64 ***
 regsvr32 %windir%\SysWOW64\capicom.dll /s
 regsvr32 %windir%\SysWOW64\msxml5.dll /s
-regsvr32 %windir%\SysWOW64\msxml5r.dll /s
 goto end
 
 :end

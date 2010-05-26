b9470308deb7b6ff78b1f8fa54679aef40d26282
julianatamizou <julianatamizou@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2010-05-26 11:16:08 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@1256 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Pacotes/Lazarus/ACBr_Boleto.pas
+++ Pacotes/Lazarus/ACBr_Boleto.pas
@@ -2,20 +2,20 @@
   This source is only used to compile and install the package.
  }
 
-unit ACBr_Boleto ; 
+unit ACBr_Boleto; 
 
 interface
 
 uses
-  ACBrBoleto, ACBrBancoBradesco, ACBrBancoBrasil, LazarusPackageIntf;
+   ACBrBoleto, ACBrBancoBradesco, ACBrBancoBrasil, LazarusPackageIntf;
 
 implementation
 
-procedure Register ; 
+procedure Register; 
 begin
-  RegisterUnit('ACBrBoleto', @ACBrBoleto.Register) ; 
-end ; 
+  RegisterUnit ( 'ACBrBoleto', @ACBrBoleto.Register ) ; 
+end; 
 
 initialization
-  RegisterPackage('ACBr_Boleto', @Register) ; 
+  RegisterPackage ( 'ACBr_Boleto', @Register ) ; 
 end.

8f061c6e24b25b742944df5c319a59cf7e70dc17
anfm <anfm@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2010-07-16 19:16:18 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@1460 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/ACBrMonitor/CLX/ACBrMonitor1.pas
+++ Projetos/ACBrMonitor/CLX/ACBrMonitor1.pas
@@ -65,7 +65,7 @@ unit ACBrMonitor1;
 interface
 
 uses
-  SysUtils, Classes, QForms, QStyle, Qt,
+  SysUtils, Classes, QForms, QStyle, Qt, Types,
   QTrayIcon,                                { Unit para criar icone no Systray }
   CmdUnit,
   ACBrECF, ACBrECFClass, ACBrDIS, ACBrGAV, ACBrGAVClass, ACBrBase, ACBrDevice, 
@@ -548,6 +548,81 @@ uses IniFiles, StrUtils, TypInfo, IdStack,
      ConfiguraSerial,
      DoECFBemafi32, DoECFObserver , DoETQUnit;
 {$R *.xfm}
+function PasswordInputBox(const ACaption, APrompt:string): string;
+var
+  Form: TForm;
+  Prompt: TLabel;
+  Edit: TEdit;
+  DialogUnits: TPointArray;
+  ButtonTop, ButtonWidth, ButtonHeight: Integer;
+  Value: string;
+  I: Integer;
+  Buffer: array[0..51] of Char;
+begin
+  Result := '';
+  Form := TForm.Create(Application);
+  with Form do
+  try
+    Canvas.Font := Font;
+    for I := 0 to 25 do Buffer[I] := Chr(I + Ord('A'));
+    for I := 0 to 25 do Buffer[I + 26] := Chr(I + Ord('a'));
+    poi
+    GetTextExtentPoint(Canvas.Handle, Buffer, 52, TSize(DialogUnits));
+    DialogUnits.X := DialogUnits.X div 52;
+    BorderStyle := bsDialog;
+    Caption := ACaption;
+    ClientWidth := MulDiv(180, DialogUnits.X, 4);
+    ClientHeight := MulDiv(63, DialogUnits.Y, 8);
+    Position := poScreenCenter;
+    Prompt := TLabel.Create(Form);
+    with Prompt do
+    begin
+      Parent := Form;
+      AutoSize := True;
+      Left := MulDiv(8, DialogUnits.X, 4);
+      Top := MulDiv(8, DialogUnits.Y, 8);
+      Caption := APrompt;
+    end;
+    Edit := TEdit.Create(Form);
+    with Edit do
+    begin
+      Parent := Form;
+      Left := Prompt.Left;
+      Top := MulDiv(19, DialogUnits.Y, 8);
+      Width := MulDiv(164, DialogUnits.X, 4);
+      MaxLength := 255;
+      PasswordChar := '*';
+      SelectAll;
+    end;
+    ButtonTop := MulDiv(41, DialogUnits.Y, 8);
+    ButtonWidth := MulDiv(50, DialogUnits.X, 4);
+    ButtonHeight := MulDiv(14, DialogUnits.Y, 8);
+    with TButton.Create(Form) do
+    begin
+      Parent := Form;
+      Caption := 'OK';
+      ModalResult := mrOk;
+      Default := True;
+      SetBounds(MulDiv(38, DialogUnits.X, 4),ButtonTop, ButtonWidth,ButtonHeight);
+    end;
+    with TButton.Create(Form) do
+    begin
+      Parent := Form;
+      Caption := 'Cancel';
+      ModalResult := mrCancel;
+      Cancel := True;
+      SetBounds(MulDiv(92, DialogUnits.X, 4),ButtonTop, ButtonWidth,ButtonHeight);
+    end;
+    if ShowModal = mrOk then
+    begin
+      Value := Edit.Text;
+      Result := Value;
+    end;
+  finally
+    Form.Free;
+    Form:=nil;  
+  end;
+end;
 
 {-------------------------------- TFrmACBrMonitor -----------------------------}
 procedure TFrmACBrMonitor.FormCreate(Sender: TObject);
@@ -1506,7 +1581,8 @@ begin
   if not SenhaOk then
   begin
      Senha := '' ;
-     if InputQuery('Configuração','Digite a Senha de Configuração',Senha) then
+//     if InputQuery('Configuração','Digite a Senha de Configuração',Senha) then
+     Senha := PasswordInputBox('Configuração','Digite a Senha de Configuração');
      begin
         Senha     := Trim(Senha) ;
         HashSenha := StringCrc16( Senha ) ;

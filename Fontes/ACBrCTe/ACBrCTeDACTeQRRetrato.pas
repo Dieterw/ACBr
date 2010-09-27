ae47fb4146929b2ddbcb9a4efcd9b9a2433e8df7
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2010-09-27 12:05:26 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@1698 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrCTe/ACBrCTeDACTeQRRetrato.pas
+++ Fontes/ACBrCTe/ACBrCTeDACTeQRRetrato.pas
@@ -568,7 +568,17 @@ begin
   PrintBand := QRCTe.PageNumber = 1 ;
 
   if Trim(FLogo) <> '' then
+   begin
    qriLogo.Picture.LoadFromFile(FLogo);
+    if FExpandirLogoMarca then
+     begin
+      qriLogo.top:=2;
+      qriLogo.Left:=2;
+      qriLogo.Height:=145;
+      qriLogo.Width:=433;
+      qriLogo.Stretch:=true;
+     end;
+   end;
 
   qrlModal.Caption := TpModalToStrText(FCTe.Ide.modal);
   qrlModelo.Caption := FCTe.Ide.modelo;

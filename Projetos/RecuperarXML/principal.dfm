8a2b227a8bad3c64c287db3ca3ccc9aee1cd9955
eselvati <eselvati@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2011-02-10 15:21:05 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@1993 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Projetos/RecuperarXML/principal.dfm
+++ Projetos/RecuperarXML/principal.dfm
@@ -1,111 +1,43 @@
 object frmPrincipal: TfrmPrincipal
-  Left = 192
-  Top = 122
-  Width = 870
-  Height = 500
+  Left = 294
+  Top = 175
+  BorderIcons = [biSystemMenu]
+  BorderStyle = bsSingle
   Caption = 'Recuperar XML NF-e'
+  ClientHeight = 395
+  ClientWidth = 649
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'MS Sans Serif'
   Font.Style = []
+  KeyPreview = True
   OldCreateOrder = False
+  Position = poScreenCenter
+  OnClose = FormClose
   OnCreate = FormCreate
-  DesignSize = (
-    854
-    464)
+  OnKeyDown = FormKeyDown
   PixelsPerInch = 96
   TextHeight = 13
-  object Image1: TImage
-    Left = 88
-    Top = 63
-    Width = 249
-    Height = 99
-  end
-  object Label1: TLabel
-    Left = 8
-    Top = 8
-    Width = 57
-    Height = 13
-    Caption = 'Chave NF-e'
-  end
-  object Label2: TLabel
-    Left = 8
-    Top = 48
-    Width = 163
-    Height = 13
-    Caption = 'Digite o c'#243'digo da imagem ao lado'
-  end
   object lblStatus: TLabel
-    Left = 118
-    Top = 173
-    Width = 110
+    Left = 0
+    Top = 158
+    Width = 649
     Height = 13
+    Alignment = taCenter
+    AutoSize = False
     Caption = 'Conectando ao SEFAZ'
     FocusControl = ProgressBar1
     Visible = False
   end
-  object btnPegarHTML: TButton
-    Left = 8
-    Top = 96
-    Width = 73
-    Height = 25
-    Caption = 'Pegar HTML'
-    Enabled = False
-    TabOrder = 3
-    OnClick = btnPegarHTMLClick
-  end
-  object edtCaptcha: TEdit
-    Left = 8
-    Top = 63
-    Width = 73
-    Height = 21
-    TabOrder = 2
-  end
-  object edtChaveNFe: TEdit
-    Left = 8
-    Top = 24
-    Width = 329
-    Height = 21
-    TabOrder = 1
-  end
   object ProgressBar1: TProgressBar
-    Left = 8
-    Top = 192
-    Width = 335
+    Left = 1
+    Top = 176
+    Width = 644
     Height = 17
-    TabOrder = 6
+    TabOrder = 1
     Visible = False
-  end
-  object Memo2: TMemo
-    Left = 8
-    Top = 216
-    Width = 337
-    Height = 233
-    Anchors = [akLeft, akTop, akBottom]
-    ScrollBars = ssBoth
-    TabOrder = 5
-  end
-  object btnNovaConsulta: TButton
-    Left = 8
-    Top = 128
-    Width = 75
-    Height = 25
-    Caption = 'Nova Consulta'
-    Enabled = False
-    TabOrder = 4
-    OnClick = btnNovaConsultaClick
-  end
-  object btnGerarXML: TButton
-    Left = 8
-    Top = 160
-    Width = 75
-    Height = 25
-    Caption = 'Gerar XML'
-    Enabled = False
-    TabOrder = 7
-    OnClick = btnGerarXMLClick
   end
   object WebBrowser1: TWebBrowser
     Left = 280
@@ -123,26 +55,162 @@ object frmPrincipal: TfrmPrincipal
       8000000000000000000000000000000000000000000000000000000000000000
       00000000000000000100000000000000000000000000000000000000}
   end
-  object Panel1: TPanel
-    Left = 352
-    Top = 8
-    Width = 497
-    Height = 441
-    Anchors = [akLeft, akTop, akRight, akBottom]
-    TabOrder = 8
-    object WBXML: TWebBrowser
-      Left = 1
-      Top = 1
-      Width = 495
-      Height = 439
-      Align = alClient
+  object Panel2: TPanel
+    Left = 0
+    Top = 0
+    Width = 351
+    Height = 101
+    TabOrder = 2
+    object Label1: TLabel
+      Left = 8
+      Top = 8
+      Width = 155
+      Height = 13
+      Caption = 'Chave de acesso da nota fiscal: '
+    end
+    object Label2: TLabel
+      Left = 8
+      Top = 48
+      Width = 169
+      Height = 13
+      Caption = 'Digite o c'#243'digo da imagem ao lado: '
+    end
+    object edtChaveNFe: TEdit
+      Left = 8
+      Top = 24
+      Width = 329
+      Height = 21
       TabOrder = 0
-      ControlData = {
-        4C000000293300005F2D00000000000000000000000000000000000000000000
-        000000004C000000000000000000000001000000E0D057007335CF11AE690800
-        2B2E126208000000000000004C0000000114020000000000C000000000000046
-        8000000000000000000000000000000000000000000000000000000000000000
-        00000000000000000100000000000000000000000000000000000000}
+    end
+    object edtCaptcha: TEdit
+      Left = 8
+      Top = 63
+      Width = 73
+      Height = 21
+      TabOrder = 1
+    end
+  end
+  object Panel3: TPanel
+    Left = 0
+    Top = 104
+    Width = 351
+    Height = 49
+    TabOrder = 3
+    object btnPegarHTML: TButton
+      Left = 114
+      Top = 14
+      Width = 76
+      Height = 27
+      Caption = 'Pegar HTML'
+      Enabled = False
+      TabOrder = 0
+      OnClick = btnPegarHTMLClick
+    end
+    object btnNovaConsulta: TButton
+      Left = 192
+      Top = 14
+      Width = 76
+      Height = 27
+      Caption = 'Nova Consulta'
+      Enabled = False
+      TabOrder = 1
+      OnClick = btnNovaConsultaClick
+    end
+    object btnGerarXML: TButton
+      Left = 270
+      Top = 14
+      Width = 76
+      Height = 27
+      Caption = 'Gerar XML'
+      Enabled = False
+      TabOrder = 2
+      OnClick = btnGerarXMLClick
+    end
+    object Button1: TButton
+      Left = 4
+      Top = 14
+      Width = 76
+      Height = 27
+      Caption = 'Salvar XML'
+      TabOrder = 3
+      OnClick = Button1Click
+    end
+  end
+  object Panel4: TPanel
+    Left = 353
+    Top = -3
+    Width = 293
+    Height = 156
+    TabOrder = 4
+    object Image1: TImage
+      Left = 21
+      Top = 27
+      Width = 249
+      Height = 99
+      Center = True
+    end
+    object Label3: TLabel
+      Left = 1
+      Top = 130
+      Width = 291
+      Height = 25
+      Cursor = crHandPoint
+      Alignment = taCenter
+      AutoSize = False
+      Caption = 'Clique aqui caso n'#227'o consiga visualizar a imagem'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clBlue
+      Font.Height = -9
+      Font.Name = 'Verdana'
+      Font.Style = [fsUnderline]
+      ParentFont = False
+      OnClick = Label3Click
+    end
+  end
+  object PageControl1: TPageControl
+    Left = 4
+    Top = 200
+    Width = 643
+    Height = 193
+    ActivePage = TabSheet1
+    TabOrder = 5
+    object TabSheet1: TTabSheet
+      Caption = 'Dados HTML'
+      object Memo2: TMemo
+        Left = 0
+        Top = 0
+        Width = 635
+        Height = 165
+        Align = alClient
+        ScrollBars = ssBoth
+        TabOrder = 0
+      end
+    end
+    object TabSheet2: TTabSheet
+      Caption = 'Dados XML'
+      ImageIndex = 1
+      object Panel1: TPanel
+        Left = 0
+        Top = 0
+        Width = 635
+        Height = 165
+        Align = alClient
+        TabOrder = 0
+        object WBXML: TWebBrowser
+          Left = 1
+          Top = 1
+          Width = 633
+          Height = 163
+          Align = alClient
+          TabOrder = 0
+          ControlData = {
+            4C0000006C410000D91000000000000000000000000000000000000000000000
+            000000004C000000000000000000000001000000E0D057007335CF11AE690800
+            2B2E126208000000000000004C0000000114020000000000C000000000000046
+            8000000000000000000000000000000000000000000000000000000000000000
+            00000000000000000100000000000000000000000000000000000000}
+        end
+      end
     end
   end
 end
--- Projetos/RecuperarXML/principal.pas
+++ Projetos/RecuperarXML/principal.pas
@@ -2,27 +2,35 @@ unit principal;
 
 interface
 
-uses UrlMon, JvGif, MSHtml, ACBrUtil, pcnAuxiliar,
+uses GifImage ,UrlMon, MSHtml, ACBrUtil, pcnAuxiliar,
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, OleCtrls, SHDocVw, ExtCtrls, ComCtrls, WinInet;
+  Dialogs, StdCtrls, OleCtrls, SHDocVw, ExtCtrls, ComCtrls, WinInet, Menus;
 
 
 type
   TfrmPrincipal = class(TForm)
     WebBrowser1: TWebBrowser;
-    btnPegarHTML: TButton;
-    edtCaptcha: TEdit;
-    Image1: TImage;
-    edtChaveNFe: TEdit;
-    Label1: TLabel;
-    Label2: TLabel;
     ProgressBar1: TProgressBar;
-    Memo2: TMemo;
-    btnNovaConsulta: TButton;
     lblStatus: TLabel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    Image1: TImage;
+    btnPegarHTML: TButton;
+    btnNovaConsulta: TButton;
     btnGerarXML: TButton;
+    Label1: TLabel;
+    edtChaveNFe: TEdit;
+    Label2: TLabel;
+    edtCaptcha: TEdit;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    Memo2: TMemo;
     Panel1: TPanel;
     WBXML: TWebBrowser;
+    Button1: TButton;
+    Label3: TLabel;
     procedure btnPegarHTMLClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure WebBrowser1DocumentComplete(Sender: TObject;
@@ -31,13 +39,22 @@ type
       ProgressMax: Integer);
     procedure btnNovaConsultaClick(Sender: TObject);
     procedure btnGerarXMLClick(Sender: TObject);
+    procedure FormKeyDown(Sender: TObject; var Key: Word;
+      Shift: TShiftState);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure Button1Click(Sender: TObject);
+    procedure Label3Click(Sender: TObject);
   private
     { Private declarations }
     function DownloadFile(SourceFile, DestFile: string): Boolean;
     function StripHTML(S: string): string;
     procedure DeleteIECache;
+    procedure PegarHTML;
+    procedure GeraXml;
+    procedure NovaConsulta;
   public
     { Public declarations }
+    FPath: string;
   end;
 
 var
@@ -113,6 +130,11 @@ end;
 
 procedure TfrmPrincipal.btnPegarHTMLClick(Sender: TObject);
 begin
+  PegarHTML;
+end;
+
+procedure TfrmPrincipal.PegarHTML;
+begin
   edtChaveNFe.Text := OnlyNumber(edtChaveNFe.Text);
 
   if not ValidarChave('NFe'+edtChaveNFe.Text) then
@@ -143,11 +165,12 @@ begin
      btnNovaConsulta.Enabled := True;
      raise;
   end;
+  PageControl1.ActivePageIndex:=0;
 end;
 
 procedure TfrmPrincipal.FormCreate(Sender: TObject);
 begin
-  WebBrowser1.Navigate('https://www.nfe.fazenda.gov.br/portal/FormularioDePesquisa.aspx?tipoconsulta=completa');
+  NovaConsulta;
 end;
 
 procedure TfrmPrincipal.WebBrowser1DocumentComplete(Sender: TObject;
@@ -159,62 +182,60 @@ var
 begin
   Application.ProcessMessages;
   if WebBrowser1.LocationURL = 'https://www.nfe.fazenda.gov.br/portal/FormularioDePesquisa.aspx?tipoconsulta=completa' then
-   begin
-     for k := 0 to WebBrowser1.OleObject.Document.Images.Length - 1 do
-      begin
-        Source := WebBrowser1.OleObject.Document.Images.Item(k).Src;
-        if (Source = 'https://www.nfe.fazenda.gov.br/scripts/srf/intercepta/captcha.aspx?opt=image') then
-         begin
-           dest := ExtractFilePath(ParamStr(0)) + 'captcha.gif';
-           DownloadFile(Source, dest);
-         end;
-      end;
+  begin
+    for k := 0 to WebBrowser1.OleObject.Document.Images.Length - 1 do
+     begin
+       Source := WebBrowser1.OleObject.Document.Images.Item(k).Src;
+       if (Source = 'https://www.nfe.fazenda.gov.br/scripts/srf/intercepta/captcha.aspx?opt=image') then
+       begin
+         dest := ExtractFilePath(ParamStr(0)) + 'captcha.gif';
+         DownloadFile(Source, dest);
+       end;
+     end;
      Image1.Picture.LoadFromFile(dest);
      btnPegarHTML.Enabled := True;
-   end
+  end
   else if WebBrowser1.LocationURL = 'https://www.nfe.fazenda.gov.br/portal/visualizacaoNFe/completa/Default.aspx' then
-   begin
-      WebBrowser1.Navigate('https://www.nfe.fazenda.gov.br/PORTAL/visualizacaoNFe/completa/impressao.aspx');
-   end
+  begin
+    WebBrowser1.Navigate('https://www.nfe.fazenda.gov.br/PORTAL/visualizacaoNFe/completa/impressao.aspx');
+  end
   else if WebBrowser1.LocationURL = 'https://www.nfe.fazenda.gov.br/PORTAL/visualizacaoNFe/completa/impressao.aspx' then
-   begin
-      textoNFe := WebBrowser1.Document as IHTMLDocument2;
-      repeat
-         Application.ProcessMessages;
-      until Assigned(textoNFe.body);
-      Memo2.Lines.Text := StripHTML(textoNFe.body.innerHTML);
-      Memo2.Lines.Text := StringReplace(Memo2.Lines.Text,'&nbsp;','',[rfReplaceAll, rfIgnoreCase]);
+  begin
+    textoNFe := WebBrowser1.Document as IHTMLDocument2;
+    repeat
+       Application.ProcessMessages;
+    until Assigned(textoNFe.body);
+    Memo2.Lines.Text := StripHTML(textoNFe.body.innerHTML);
+    Memo2.Lines.Text := StringReplace(Memo2.Lines.Text,'&nbsp;','',[rfReplaceAll, rfIgnoreCase]);
 
-      i:= 0;
-      while i < memo2.Lines.Count-1 do
-       begin
-         if trim(Memo2.Lines[i]) = '' then
-          begin
-            Memo2.Lines.Delete(i);
-            i := i - 1;
-          end;
-         i := i + 1;
-       end;
-     Image1.Picture := nil;
-     btnGerarXML.Enabled := True;
-     btnNovaConsulta.Enabled := True;
-   end
+    i:= 0;
+    while i < memo2.Lines.Count-1 do
+    begin
+      if trim(Memo2.Lines[i]) = '' then
+      begin
+        Memo2.Lines.Delete(i);
+        i := i - 1;
+      end;
+      i := i + 1;
+    end;
+    Image1.Picture := nil;
+    btnGerarXML.Enabled := True;
+    GeraXml;
+  end
   else if WebBrowser1.LocationURL = 'https://www.nfe.fazenda.gov.br/portal/inexistente_completa.aspx' then
-   begin
-     MessageDlg('NF-e INEXISTENTE na base nacional, favor consultar esta NF-e no site da SEFAZ de origem.',mtError,[mbok],0);
-     Image1.Picture := nil;
-     btnGerarXML.Enabled := True;
-     btnNovaConsulta.Enabled := True;
-   end
+  begin
+    MessageDlg('NF-e INEXISTENTE na base nacional, favor consultar esta NF-e no site da SEFAZ de origem.',mtError,[mbok],0);
+    Image1.Picture := nil;
+    btnGerarXML.Enabled := True;
+    btnNovaConsulta.Enabled := True;
+  end
   else
-   begin
-   begin
-     MessageDlg('Erro carregando URL: '+WebBrowser1.LocationURL,mtError,[mbok],0);
-     Image1.Picture := nil;
-     btnGerarXML.Enabled := True;
-     btnNovaConsulta.Enabled := True;
-   end
-   end;
+  begin
+    MessageDlg('Erro carregando URL: '+WebBrowser1.LocationURL,mtError,[mbok],0);
+    Image1.Picture := nil;
+    btnGerarXML.Enabled := True;
+    btnNovaConsulta.Enabled := True;
+  end;
 end;
 
 procedure TfrmPrincipal.WebBrowser1ProgressChange(Sender: TObject; Progress,
@@ -248,16 +269,54 @@ end;
 
 procedure TfrmPrincipal.btnNovaConsultaClick(Sender: TObject);
 begin
+  NovaConsulta;
+end;
+
+procedure TfrmPrincipal.NovaConsulta;
+begin
   btnNovaConsulta.Enabled := False;
   btnGerarXML.Enabled := False;
-DeleteIECache;
+  DeleteIECache;
   Memo2.Lines.Clear;
   WebBrowser1.Navigate('https://www.nfe.fazenda.gov.br/portal/FormularioDePesquisa.aspx?tipoconsulta=completa');
 end;
 
 procedure TfrmPrincipal.btnGerarXMLClick(Sender: TObject);
 begin
-  WBXML.Navigate(GerarXML(Memo2.Lines.Text));
+GeraXML;
+end;
+
+procedure TfrmPrincipal.GeraXml;
+begin
+  FPath:=GerarXML(Memo2.Lines.Text);
+  WBXML.Navigate(FPath);
+  MessageBox(0,PChar('XML '+FPath+' gerado com sucesso!'),'Informação',
+    MB_ICONINFORMATION+MB_TASKMODAL);
+  btnNovaConsulta.Enabled:=True;
+  btnPegarHTML.Enabled:=True;
+end;
+
+procedure TfrmPrincipal.FormKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+if Key=vk_escape then
+  Close;
+end;
+
+procedure TfrmPrincipal.FormClose(Sender: TObject;
+  var Action: TCloseAction);
+begin
+Action:=caFree;
+end;
+
+procedure TfrmPrincipal.Button1Click(Sender: TObject);
+begin
+PegarHTML;
+end;
+
+procedure TfrmPrincipal.Label3Click(Sender: TObject);
+begin
+NovaConsulta;
 end;
 
 end.

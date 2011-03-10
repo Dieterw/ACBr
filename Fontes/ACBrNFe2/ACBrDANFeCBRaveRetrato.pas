4d9f3e6c82535c56dd98af5665b481cca64adc95
juaumkiko <juaumkiko@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2011-03-10 19:44:03 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@2053 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrNFe2/ACBrDANFeCBRaveRetrato.pas
+++ Fontes/ACBrNFe2/ACBrDANFeCBRaveRetrato.pas
@@ -1244,8 +1244,8 @@ var wtemp: double;
 begin
   //tamanho padrao das colunas
   ColsWidth[1]:=15;
-  ColsWidth[2]:=54;
-  ColsWidth[3]:=11;
+  ColsWidth[2]:=52;
+  ColsWidth[3]:=13;
   ColsWidth[4]:=7;
   ColsWidth[5]:=7;
   ColsWidth[6]:=7;

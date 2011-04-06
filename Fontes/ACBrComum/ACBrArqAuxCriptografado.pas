7fa418dbdb50545ecda4952b87ec3ed4ab2e997c
regyssilveira <regyssilveira@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2011-04-06 20:13:17 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@2140 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrComum/ACBrArqAuxCriptografado.pas
+++ Fontes/ACBrComum/ACBrArqAuxCriptografado.pas
@@ -136,7 +136,7 @@ begin
     raise EACBrArqAux_NomeArquivo.Create('Nome do arquivo criptografado auxiliar não informado.');
 
   Result :=
-    IncludeTrailingPathDelimiter(ExtractFilePath(FPath)) +
+    IncludeTrailingPathDelimiter(FPath) +
     ExtractFileName(FArquivo);
 end;
 

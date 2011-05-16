0337cfb80490a154d06ce210309e3b0d9f065d33
ispinheiro <ispinheiro@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2011-05-16 03:56:09 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@2356 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Exemplos/ACBrInStore/Delphi/Unit2.pas
+++ Exemplos/ACBrInStore/Delphi/Unit2.pas
@@ -50,15 +50,28 @@ end;
 
 procedure TForm2.Button1Click(Sender: TObject);
 begin
+  // Define a mascara do código de barra da balança, e já acha o prefixo
+  // Guardando na propriedade Prefixo.
   ACBrInStore1.Codificacao := edtCodificacao.Text;
 
-  ACBrInStore1.Desmembrar(edtCodigoEtiqueta.Text);
+  // Checa se o código da balança tem 13 digitos
+  if Length(edtCodigoEtiqueta.Text) = 13 then
+  begin
+     // Verifica se o prefixo encontrado na mascara é igual ao 
+     // prefixo do código de barra que veio da balança
+     // Atendendo a essas situações isso quer dizer que o código recebido é
+     // um código gerado pela balança.
+     if ACBrInStore1.Prefixo = Copy(edtCodigoEtiqueta.Text, 1, Length(ACBrInStore1.Prefixo)) then
+     begin
+        ACBrInStore1.Desmembrar(edtCodigoEtiqueta.Text);
 
-  edtPrefixo.Text := ACBrInStore1.Prefixo;
-  edtCodigo.Text  := ACBrInStore1.Codigo;
-  edtPeso.Text    := FloatToStr( ACBrInStore1.Peso );
-  edtTotal.Text   := FloatToStr( ACBrInStore1.Total );
-  edtDV.Text      := ACBrInStore1.DV;
+        edtPrefixo.Text := ACBrInStore1.Prefixo;
+        edtCodigo.Text  := ACBrInStore1.Codigo;
+        edtPeso.Text    := FloatToStr( ACBrInStore1.Peso );
+        edtTotal.Text   := FloatToStr( ACBrInStore1.Total );
+        edtDV.Text      := ACBrInStore1.DV;
+     end;
+  end;
 end;
 
 end.

6aa3a69bdd84f509aa996a85cd127db53562048a
regyssilveira <regyssilveira@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2011-07-18 16:48:50 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@2523 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrSerial/ACBrECFBematech.pas
+++ Fontes/ACBrSerial/ACBrECFBematech.pas
@@ -1799,7 +1799,7 @@ begin
        FPagto := TACBrECFFormaPagamento.create ;
 
        FPagto.Indice    := IntToStrZero(Cont,2) ;
-       FPagto.Descricao := Descr ;
+       FPagto.Descricao := DecodificarPaginaDeCodigoECF( Descr );
        FPagto.PermiteVinculado := (Cont > 1); {Apenas 1-Dinheiro nao permite}
        FPagto.Total := RoundTo( StrToFloatDef( BcdToAsc(
                            copy(StrRet,(Cont*10) - 9 + 833,10) ),0) / 10000, -4) ;

6246b6e219719511555576cf63638e0509022093
EMBarbosa <EMBarbosa@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2011-11-22 13:35:10 +0000

    Campo IND_PROC do Registro F129 estava gerando dados incorretos
    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@3012 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrSPED/ACBrSPEDPisCofins/ACBrEPCBloco_F_Class.pas
+++ Fontes/ACBrSPED/ACBrSPEDPisCofins/ACBrEPCBloco_F_Class.pas
@@ -745,8 +745,8 @@ begin
       with RegF120.RegistroF129.Items[intFor] do
       begin
         case IND_PROC of
-          opJusticaFederal : strIND_PROC := '0';
-          opSecexRFB       : strIND_PROC := '1';
+          opJusticaFederal : strIND_PROC := '1';
+          opSecexRFB       : strIND_PROC := '3';
           opOutros         : strIND_PROC := '9';
           opNenhum         : strIND_PROC := '';
         end;

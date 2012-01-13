10fb64dd6382a35b1d85fd734ef2ecbf527ef746
dopi <dopi@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2012-01-13 18:26:27 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@3178 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Pacotes/Lazarus/PCN.pas
+++ Pacotes/Lazarus/PCN.pas
@@ -2,18 +2,17 @@
   This source is only used to compile and install the package.
  }
 
-unit PCN; 
+unit PCN ; 
 
 interface
 
 uses
-    pcnAuxiliar, pcnCabecalho, pcnCadEmiDFe, pcnCancNFe, pcnConsCad, 
+  pcnAuxiliar, pcnCabecalho, pcnCadEmiDFe, pcnCancNFe, pcnConsCad, 
   pcnConsDPEC, pcnConsReciNFe, pcnConsSitNFe, pcnConsStatServ, pcnConversao, 
   pcnEnvDPEC, pcnEnvNFe, pcnGerador, pcnInutNFe, pcnLayoutTXT, pcnLeitor, 
-  pcnModeloNFe, pcnNFe, pcnNFeR, pcnNFeRTXT, pcnNFeW, pcnProcNFe, 
-  pcnRetAtuCadEmiDFe, pcnRetCancNFe, pcnRetConsCad, pcnRetConsReciNFe, 
-  pcnRetConsSitNFe, pcnRetConsStatServ, pcnRetDPEC, pcnRetEnvNFe, 
-  pcnRetInutNFe, pcnSignature, ACBrHTMLtoXML;
+  pcnNFe, pcnNFeR, pcnNFeRTXT, pcnNFeW, pcnProcNFe, pcnRetAtuCadEmiDFe, 
+  pcnRetCancNFe, pcnRetConsCad, pcnRetConsReciNFe, pcnRetConsSitNFe, 
+  pcnRetConsStatServ, pcnRetDPEC, pcnRetEnvNFe, pcnRetInutNFe, pcnSignature;
 
 implementation
 

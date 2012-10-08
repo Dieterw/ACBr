02e3613c0e28b2813a2438d463600dbcc33a1d8b
julianatamizou <julianatamizou@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2012-10-08 12:50:10 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@4064 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrBoleto/ACBrBancoBrasil.pas
+++ Fontes/ACBrBoleto/ACBrBancoBrasil.pas
@@ -440,7 +440,8 @@ begin
                IntToStrZero( round(ValorIOF * 100), 15)                   + //166 a 180 - Valor do IOF a ser recolhido
                IntToStrZero( round(ValorAbatimento * 100), 15)            + //181 a 195 - Valor do abatimento
                padL(SeuNumero, 25, ' ')                                   + //196 a 220 - Identificação do título na empresa
-               IfThen((DataProtesto <> null) and (DataProtesto > Vencimento), '1', '3') + //221 - Código de protesto: Protestar em XX dias corridos
+               IfThen((DataProtesto <> null) and (DataProtesto > Vencimento),
+                      IfThen((DaySpan(Vencimento, DataProtesto) > 5), '1', '2'), '3')       + //221 - Código de protesto: Protestar em XX dias corridos
                IfThen((DataProtesto <> null) and (DataProtesto > Vencimento),
                     padR(IntToStr(DaysBetween(DataProtesto, Vencimento)), 2, '0'), '00') + //222 a 223 - Prazo para protesto (em dias corridos)
                '0'                                                        + //224 - Campo não tratado pelo BB [ Alterado conforme instruções da CSO Brasília ] {27-07-09}

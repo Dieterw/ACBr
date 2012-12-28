73d2666dbf9a1bb3a5ee00555b95f16442a043d3
italojjr <italojjr@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2012-12-28 16:47:23 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@4576 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

--- Fontes/ACBrNFSe/ACBrProvedorBetim.pas
+++ Fontes/ACBrNFSe/ACBrProvedorBetim.pas
@@ -454,7 +454,7 @@ var
 begin
  AXML := '<?xml version="1.0" encoding="UTF-8"?>' +
          DadosMsg;
- AXML := StringReplace(StringReplace(AXML, '<', '&lt;', [rfReplaceAll]), '>', '&gt;', [rfReplaceAll]) +
+ AXML := StringReplace(StringReplace(AXML, '<', '&lt;', [rfReplaceAll]), '>', '&gt;', [rfReplaceAll]);
 
  result := '<?xml version="1.0" encoding="UTF-8"?>' +
            '<S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/" ' +

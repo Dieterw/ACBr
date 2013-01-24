6561026a6168eafea85c021ee46f88520f4b223a
julianatamizou <julianatamizou@6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6>
2013-01-24 19:18:07 +0000

    git-svn-id: https://acbr.svn.sourceforge.net/svnroot/acbr/trunk@4679 6e92efe7-b92a-0410-a9ec-f9e4e41bb3a6

---
+++ Pacotes/Lazarus/ACBrLFD.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit ACBrLFD;
+
+interface
+
+uses
+  ACBrLFDBloco_0, ACBrLFDBloco_0_Class, ACBrLFDBlocos, ACBrSped, 
+  LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('ACBrLFD', @Register);
+end.
